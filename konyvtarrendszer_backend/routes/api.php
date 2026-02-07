f2a4ad90b7cc6e86bd7caefa8b128c6dc84af296
harycsi <haragos.csilla@gmail.com>
2026-02-07 15:45:46 +0100

    continous...

--- konyvtarrendszer_backend/routes/api.php
+++ konyvtarrendszer_backend/routes/api.php
@@ -9,14 +9,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/foglalasok", [FoglalasController::class, "index"]);
-Route::get("/foglalasok/{user_id}", [FoglalasController::class, "show"]);
-Route::post("/foglalasok", [FoglalasController::class, "store"]);
-Route::put("/foglalasok/{user_id}", [FoglalasController::class, "update"]);
-Route::delete("/foglalasok/{user_id}", [FoglalasController::class, "destroy"]);
+Route::get("/foglalas", [FoglalasController::class, "index"]);
+Route::get("/foglalas/{user_id}", [FoglalasController::class, "show"]);
+Route::post("/foglalas", [FoglalasController::class, "store"]);
+Route::put("/foglalas/{user_id}", [FoglalasController::class, "update"]);
+Route::delete("/foglalas/{user_id}", [FoglalasController::class, "destroy"]);
 
-Route::get("/kolcsonzesek", [KolcsonzesController::class, "index"]);
-Route::get("/kolcsonzesek/{user_id}", [KolcsonzesController::class, "show"]);
-Route::post("/kolcsonzesek", [KolcsonzesController::class, "store"]);
-Route::delete("/kolcsonzesek/{user_id}", [KolcsonzesController::class, "destroy"]);
+Route::get("/kolcsonzes", [KolcsonzesController::class, "index"]);
+Route::get("/kolcsonzes/{user_id}", [KolcsonzesController::class, "show"]);
+Route::post("/kolcsonzes", [KolcsonzesController::class, "store"]);
+Route::delete("/kolcsonzes/{user_id}", [KolcsonzesController::class, "destroy"]);
 
--- konyvtarrendszer_frontend/src/App.tsx
+++ konyvtarrendszer_frontend/src/App.tsx
@@ -1,65 +1,128 @@
 import { useState, PropsWithChildren } from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { text } from 'stream/consumers';
 
 const LibraryProvider = ({ children }: PropsWithChildren) => {
 
-
   const contextValue = {
-
 
   }
 
   return <>
-    
+
   </>
 
 }
 
-function UsernameInput() {
+function UserLogin() {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState("");
 
-  return (
+  return <>
+    <h1>Könyvtár</h1>
+    <form>
+    <label>Bejelentkezés
+    <div>
+    <label>Felhasználó név:</label>
     <input
       type="text"
       value={username}
       onChange={(e) => setUsername(e.target.value)}
     />
-  );
-};
-
-function PasswordInput() {
-  const [password, setPassword] = useState("");
-
-  return (
+    </div>
+    <div>
+    <label>Jelszó:</label>
     <input
       type="password"
       value={password}
       onChange={(e) => setPassword(e.target.value)}
     />
-  );
+    </div>
+    </label>
+    <button type='button'>Belépés</button>
+    </form>
+  </>
+}
+
+function UserRegister() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+
+  return <>
+    <h1>Könyvtár</h1>
+    <form>
+    <label>Regisztráció
+    <div>
+    <label>Teljes név:</label>
+     <input
+      type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
+    </div>
+    <div>
+    <label>Felhasználó név:</label>
+    <input
+      type="text"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+    />
+    </div>
+    <div>
+    <label>Jelszó:</label>
+    <input
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+    />
+    </div>
+    <div>
+    <label>Email cím:</label>
+    <input
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+    />
+    </div>
+    <div>
+    <label>Lakcím:</label>
+    <input
+      type="text"
+      value={address}
+      onChange={(e) => setAddress(e.target.value)}
+    />
+    </div>
+    <div>
+    <label>Telefonszám:</label>
+     <input
+      type="tel"
+      value={phone}
+      onChange={(e) => setPhone(e.target.value)}
+    />
+    </div>
+    </label>
+    <button type='submit'>Regisztráció</button>
+    </form>
+  </>
 }
 
 function App() {
+
   return (
     <LibraryProvider>
       <BrowserRouter>
         <div className="App">
           <header className="App-header">
-            <h1>Könyvtári rendszer</h1>
-            <h2>Könyvtáros felülete</h2>
-            <label>Bejelentkezés
-              <p>Felhasználó név:</p>
-              <UsernameInput></UsernameInput>
-              <p>Jelszó:</p>
-              <PasswordInput></PasswordInput>
-            </label>
-            <button type="submit">Belépés</button>
           </header>
         </div >
-
         <Routes>
-          <Route path="/login" element={<> <UsernameInput /> && <PasswordInput /> </>} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/register" element={<UserRegister />} />
         </Routes>
       </BrowserRouter>
     </LibraryProvider>
